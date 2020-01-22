283074d0498510c5841386f5f4ae36f361dc4e73
Donald E. Willcox <eugene.willcox@gmail.com>
2020-01-21 18:51:51 -0800

    Rename damping function, damp in domain [0, R_star-dR_star]. Normalize argument to tanh in damping formula.

--- Source/Evolve/WarpXEvolveEM.cpp
+++ Source/Evolve/WarpXEvolveEM.cpp
@@ -149,15 +149,15 @@ WarpX::EvolveEM (int numsteps)
                     amrex::ParallelFor(tex, tey, tez,
                     [=] AMREX_GPU_DEVICE (int i, int j, int k)
                     {
-                        PulsarParm::PulsarDampEField(i, j, k, geom, Exfab);
+                        PulsarParm::DampEField(i, j, k, geom, Exfab);
                     },
                     [=] AMREX_GPU_DEVICE (int i, int j, int k)
                     {
-                        PulsarParm::PulsarDampEField(i, j, k, geom, Eyfab);
+                        PulsarParm::DampEField(i, j, k, geom, Eyfab);
                     },
                     [=] AMREX_GPU_DEVICE (int i, int j, int k)
                     {
-                        PulsarParm::PulsarDampEField(i, j, k, geom, Ezfab);
+                        PulsarParm::DampEField(i, j, k, geom, Ezfab);
                     });
                 }
             }
