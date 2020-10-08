4b3b0acf087755145f2694ae808e84f781084224
jackiezy <58234082+jackieyao0114@users.noreply.github.com>
2020-10-08 14:30:42 -0400

    indention fix again

--- Source/FieldSolver/FiniteDifferenceSolver/MacroscopicEvolveM.cpp
+++ Source/FieldSolver/FiniteDifferenceSolver/MacroscopicEvolveM.cpp
@@ -172,8 +172,7 @@ void FiniteDifferenceSolver::MacroscopicEvolveMCartesian(
                     }
                     else if (M_normalization == 0) {
                         // check the normalized error
-                        if (M_magnitude_normalized > 1._rt + mag_normalized_error)
-                        {
+                        if (M_magnitude_normalized > 1._rt + mag_normalized_error) {
                             printf("i = %d, j=%d, k=%d\n", i, j, k);
                             printf("M_magnitude_normalized = %f, Ms = %f\n", M_magnitude_normalized, mag_Ms_arrx(i, j, k));
                             amrex::Abort("Caution: Unsaturated material has M_xface exceeding the saturation magnetization");
